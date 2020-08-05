500dc88d0baf88a8cdf0b4f570acc6fb256cb272
CSinensis <melinda.chen16@gmail.com>
2020-08-05 18:54:58 -0400

    Added template for clickable menu

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__, static_url_path='/', static_folder='')
 def root():
     return app.send_static_file('index.html')
 
+@app.route('/home')
+def home():
+    return app.send_static_file('home.html')
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
 
