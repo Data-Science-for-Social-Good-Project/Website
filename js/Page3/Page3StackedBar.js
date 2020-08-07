5923ec0aac556a1aea4b8e550b987cd9b094237c
CSinensis <melinda.chen16@gmail.com>
2020-08-07 13:46:12 -0400

    Added Stacked bar chart to page 3

---
+++ js/Page3/Page3StackedBar.js
@@ -0,0 +1,50 @@
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+var ctx = document.getElementById("stackedBar").getContext('2d');
+
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["True","False","Non-Verifiable"],
+        datasets: [{
+            label: "CovidMemes3",
+            backgroundColor: "#caf270",
+            data: [19,8,9],
+        }, {
+            label: "Coronavirus Memes for Self-Isolating Teens",
+            backgroundColor: "#45c490",
+            data: [4,1,5],
+        }, {
+            label: "General Search",
+            backgroundColor: "#008d93",
+            data: [14,1,1],
+        }],
+    },
+options: {
+    tooltips: {
+      displayColors: true,
+      callbacks:{
+        mode: 'x',
+      },
+    },
+    scales: {
+      xAxes: [{
+        stacked: true,
+        gridLines: {
+          display: false,
+        }
+      }],
+      yAxes: [{
+        stacked: true,
+        ticks: {
+          beginAtZero: true,
+        },
+        type: 'linear',
+      }]
+    },
+        responsive: true,
+        maintainAspectRatio: true,
+        legend: { position: 'bottom' },
+    }
+});
